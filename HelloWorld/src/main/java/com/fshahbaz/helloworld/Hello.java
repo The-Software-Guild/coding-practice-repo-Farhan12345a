3b3266fdc309aeeba1aafe82a458347ec85ce148
Farhan Shahbaz <shahbazfarhan25@gmail.com>
2021-07-26 16:42:07 -0400

    WindowsMaster

--- HelloWorld/src/main/java/com/fshahbaz/helloworld/Hello.java
+++ HelloWorld/src/main/java/com/fshahbaz/helloworld/Hello.java
@@ -12,5 +12,6 @@ package com.fshahbaz.helloworld;
 public class Hello {
      public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("My name is Harry!");
     }
 }
